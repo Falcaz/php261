5681cbf47d6b77d84c474882b13195efc799e25e
Falcaz <clarafalc.ifrj@gmail.com>
2024-08-01 17:36:28 -0300

    atualizado

--- consulta.php
+++ consulta.php
@@ -12,4 +12,17 @@ if (mysqli_num_rows($resultado)>0){
 }else{ 
     echo "Nehum registro encontrado";
 }
+echo "<br>Nomes que começa com 'c'<br>";
+$sql = "SELECT id, nome, senha, email FROM usuario where nome like 'c%';";
+$resultado = mysqli_query($conexao, $sql);
+echo "<br>";
+if(mysqli_num_rows($resultado) > 0){
+    while($registro = mysqli_fetch_assoc($resultado)){
+        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
+        " " . $registro["senha"]. " - Email: " . $registro["email"].
+        "<br>";
+    }
+} else {
+    echo "Nenhum registro encontrado.";
+}
 ?>
